171f4e38e143caa0eb9ce9f9bb9004c9d4dd6419
Sergey Shlyakhov <sergey.schlyakhov@ya.ru>
2022-09-05 10:41:23 +0300

    Init

---
+++ src/test/java/com/intro/IntroductionSOAP/IntroductionSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intro.IntroductionSOAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
